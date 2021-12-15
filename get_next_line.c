bc5a11850f1ca7600656f4f92a127d053a12e085
iAkoa <thingspaozieuryt@gmail.com>
2021-12-15 18:03:05 +0100

    dsfsd

--- get_next_line.c
+++ get_next_line.c
@@ -5,163 +5,186 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmattheo <rmattheo@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/22 13:23:11 by rmattheo          #+#    #+#             */
-/*   Updated: 2021/11/29 17:47:57 by rmattheo         ###   ########lyon.fr   */
+/*   Created: 2021/12/15 17:53:03 by rmattheo          #+#    #+#             */
+/*   Updated: 2021/12/15 17:53:05 by rmattheo         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-static char	*add_line_realloc(char **buffer, int nb_letters)
+char	*ft_strchr(const char *str, int c)
 {
-	int		i;
-	int		j;
-	char	*temp;
-	char	*line;
-
-	j = 0;
-	i = 0;
-	temp = *buffer;
-	// printf("taille de la ligne = %i\n", nb_letters + 1);
-	line = ft_calloc(1, nb_letters + 1);
-	if (!line)
-	{
-		free(temp);
-		return (NULL);
-	}
-	while (j != nb_letters)
-	{
-		line[j] = temp[j];
-		j++;
-	}
-	// printf("\n\nBUFFER ajustement = %i\n\n", ft_strlen(temp) - nb_letters + 1);
-	*buffer = ft_calloc(1, ft_strlen(temp) - nb_letters + 1);
-	if (!buffer)
-	{	
-		free(temp);
-		return (NULL);
-	}
-	while (temp[i + nb_letters])
-	{
-		(*buffer)[i] = temp[nb_letters + i];
-		i++;
-	}
-	// printf("BUFFER de fonction = %s", *buffer);
-	free(temp);
-	if(*buffer[0] == '\0')
-	{
-		free(line);
-		return (NULL);
-	}
-	return (line);
+	while (*str && *str != (char)c)
+		str++;
+	if (*str == (char)c || c == 0)
+		return ((char *)str);
+	return (NULL);
 }
 
-static int	check_line(char *buffer)
+int	ft_strlen(const char *s)
 {
-	int		i;
+	int	i;
 
+	if (!s)
+		return (0);
 	i = 0;
-	if (!buffer)
-		return (0);
-	while (buffer[i])
-	{
-		if (buffer[i] == '\n' || buffer[i + 1] == '\0')
-			return (i + 1);
+	while (s[i])
 		i++;
-	}
-	return (0);
-}
-
-static int	calloc_buffer(char **buffer)
-{
-	char	*temp;
-	int		i;
-
-	i = 0;
-
-	temp = *buffer;
-	// printf("taille strlen = %i\n", ft_strlen(*buffer));
-	*buffer = ft_calloc(1, ft_strlen(*buffer) + BUFFER_SIZE + 1);
-	// printf("BUFFER = %s\n", *buffer);
-	// printf("taille calloc = %i\n", ft_strlen(*buffer) + BUFFER_SIZE + 1);
-	if (!buffer)
-	{
-		free(temp);
-		return (0);
-	}
-	if (!temp)
-		return (0);
-	while ((temp && temp[i]))
-	{
-		(*buffer)[i] = temp[i];
-		i++;
-	}
-	free(temp);
 	return (i);
 }
-
-char	*found_line(char **buffer, int fd)
+void	*ft_memcpy(void *dest, const void *src, int n)
 {
-	char	*line = NULL;
-	int		i;
-	int		octet;
-	int		nb_letters;
+	size_t	i;
 
-	i = 0;
-	while (i == 0)
-	{
-		i = calloc_buffer(buffer);
-		octet = read (fd, *buffer + i, BUFFER_SIZE);
-		nb_letters = check_line(*buffer);
-		if (octet < 0)
-			return (NULL);
-
-		if (octet == 0)
-		{	
-			if (!ft_strlen(*buffer))
-			{
-				return (NULL);
-			}
-			return (add_line_realloc(buffer, nb_letters));
-		}
-		if (nb_letters > 0)
-			return (add_line_realloc(buffer, nb_letters));
-		i = 0;
-	}
-	return (line);
+	if (!src && !dest)
+		return (NULL);
+	i = n;
+	while (n--)
+		*(unsigned char *)dest++ = *(unsigned char *)src++;
+	return (dest - i);
 }
+void	ft_bzero(void *s, size_t n)
+{
+	while (n-- > 0)
+		*(unsigned char *)s++ = 0;
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*s;
+
+	s = malloc(count * size);
+	if (!s)
+		return (NULL);
+	ft_bzero(s, (count * size));
+	return (s);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*d;
+	size_t	i;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = ft_strlen(s1) + ft_strlen(s2);
+	d = malloc(sizeof(char) * (i + 1));
+	if (!d)
+		return (NULL);
+	d[i] = 0;
+	while (*s1)
+		*d++ = *s1++;
+	while (*s2)
+		*d++ = *s2++;
+	return (d - i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*d;
+	size_t	i;
+
+	if (!s && len > 0)
+		return (NULL);
+	i = ft_strlen(&s[start]);
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) < start)
+		return (ft_calloc(1, 1));
+	if (ft_strlen(s) < len)
+		len = ft_strlen(s);
+	d = malloc((char)len + 1);
+	if (!d)
+		return (NULL);
+	d[len] = 0;
+	d = ft_memcpy(d, &s[start], len);
+	return (d);
+}
+
+void	ft_read(int fd, char **lines, char *buffer)
+{
+	char	*temp;
+	int		i;
+
+	if (!*lines)
+	{
+		i = read(fd, buffer, BUFFER_SIZE);
+		buffer[i] = '\0';
+		*lines = ft_substr(buffer, 0, BUFFER_SIZE);
+	}
+	if (!ft_strchr(*lines, '\n'))
+	{
+		i = read(fd, buffer, BUFFER_SIZE);
+		while (i > 0 && !ft_strchr(*lines, '\n'))
+		{
+			buffer[i] = '\0';
+			temp = *lines;
+			*lines = ft_strjoin(*lines, buffer);
+			free(temp);
+			if (ft_strchr(*lines, '\n'))
+				break ;
+			i = read(fd, buffer, BUFFER_SIZE);
+		}
+	}
+}
+
+char	*ft_process(char **lines)
+{
+	char	*temp;
+	char	*ret;
+	int		i;
+	int 	j;
+
+	if (!*lines)
+		return (NULL);
+	if (!ft_strchr(*lines, '\n'))
+	{
+		if (*lines[0] == '\0')
+		{
+			free(*lines);
+			*lines = NULL;
+			return (NULL);
+		}
+		ret = ft_substr(*lines, 0, ft_strlen(*lines));
+		free (*lines);
+		return (ret);
+	}
+	i = ft_strlen(*lines);
+	j = ft_strlen(ft_strchr(*lines, '\n'));
+	ret = ft_substr(*lines, 0, i - j);
+	temp = *lines;
+	*lines = ft_substr(*lines, i - j + 1, j);
+	free(temp);
+	return (ret);
+}
+
 
 char	*get_next_line(int fd)
 {
-	static char	*buffer;
-	char		*line;
-	int			i;
-
-	i = 0;
-	if (!fd)
-		return (0);
-	line = found_line(&buffer, fd);
-	if (!line)
+	char		*buffer;
+	static char	*lines;
+	
+	if (BUFFER_SIZE < 1)
 	{
-		free(buffer);
-		buffer = NULL;
+		return (NULL);
 	}
-	return (line);
+	ft_read(fd, &lines, buffer);
+	return (ft_process(&lines));
 }
 
+int	main()
+{
+	int		fd;
+	char	*line;
 
-// int	main()
-// {
-// 	int fd;
-// 	char *line;
-	
-// 	fd = open ("file.txt", O_RDONLY);
-// 	while((line = get_next_line(fd)) != NULL)
-// 	{
-// 		printf("\nline  |  %s", line);
-// 		free(line);
-// 	}
-// 	free(line);
-// 	return (0);
-// }
+	fd = open ("file.txt", O_RDONLY);
+	while((line = get_next_line(fd)))
+	{
+		printf("line  |  %s\n", line);
+		free(line);
+	}
+	close (fd);
+	free(line);
+	return (0);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pat <pat@student.42lyon.fr>                +#+  +:+       +#+        */
+/*   By: rmattheo <rmattheo@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 13:21:07 by rmattheo          #+#    #+#             */
-/*   Updated: 2021/11/26 00:36:40 by pat              ###   ########lyon.fr   */
+/*   Updated: 2021/12/02 17:09:12 by rmattheo         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,12 @@
 # include <stdlib.h>
 # include <fcntl.h>
 
-char		*get_next_line(int fd);
-void		*ft_calloc(int count, int size);
-int			ft_strlen(char *str);
-char		*ft_strchr(const char *str, int c);
-char		*ft_strcpy(char *dest, const char *src);
-char		*ft_strdup(const char *src);
-char		*found_line(char **buffer, int fd);
+typedef struct s_struct
+{
+	int		pos;
+	char	*data;
 
+}			t_struct;
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 8
